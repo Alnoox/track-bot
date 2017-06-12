93c2afe12c67754bf67eab035bdb58af6474c228
Alexandre JANNAUD <alexandre.jannaud@hotmail.fr>
2017-06-12 15:10:52 +0200

    DEBUG - Display of process time per frame

--- pytm2-1.py
+++ pytm2-1.py
@@ -3,10 +3,14 @@ from PIL import ImageGrab
 import cv2
 import time
 
+last_time = time.time()
 while(True):
     printscreen_pil = ImageGrab.grab(bbox=(0,40,800,640))
     printscreen_numpy = np.array(printscreen_pil.getdata(),dtype='uint8')\
     .reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))
+
+    print('Loop took {} seconds'.format(time.time()-last_time))
+    last_time = time.time()
     cv2.imshow('window',printscreen_numpy)
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
