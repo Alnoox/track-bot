2d55dcd95f541aae6b45b2726bece2edee56528a
Alexandre JANNAUD <alexandre.jannaud@hotmail.fr>
2017-06-12 15:18:04 +0200

    DEBUG - Optimisation of framerate

--- pytm2-1.py
+++ pytm2-1.py
@@ -6,8 +6,7 @@ import time
 last_time = time.time()
 while(True):
     printscreen_pil = ImageGrab.grab(bbox=(0,40,800,640))
-    ##printscreen_numpy = np.array(printscreen_pil.getdata(),dtype='uint8')\
-    .reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))
+    ##printscreen_numpy = np.array(printscreen_pil.getdata(),dtype='uint8')
 
     print('Loop took {} seconds'.format(time.time()-last_time))
     last_time = time.time()
