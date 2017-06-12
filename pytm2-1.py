61fb7f0654bdf32ee037da89bd2a9b2fd0e0740c
Alexandre JANNAUD <alexandre.jannaud@hotmail.fr>
2017-06-12 15:15:08 +0200

    DEBUG - Suppression of uselesse reshape function

--- pytm2-1.py
+++ pytm2-1.py
@@ -6,12 +6,12 @@ import time
 last_time = time.time()
 while(True):
     printscreen_pil = ImageGrab.grab(bbox=(0,40,800,640))
-    printscreen_numpy = np.array(printscreen_pil.getdata(),dtype='uint8')\
+    ##printscreen_numpy = np.array(printscreen_pil.getdata(),dtype='uint8')\
     .reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))
 
     print('Loop took {} seconds'.format(time.time()-last_time))
     last_time = time.time()
-    cv2.imshow('window',printscreen_numpy)
+    cv2.imshow('window',np.array(printscreen_pil))
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
