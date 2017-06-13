5314433d0936fc1327780998da6825cc062c2162
Alexandre JANNAUD <alexandre.jannaud@hotmail.fr>
2017-06-13 15:08:01 +0200

    DEBUG - Refactor fonction calls and var names

--- pytm2-1.py
+++ pytm2-1.py
@@ -5,12 +5,12 @@ import time
 
 last_time = time.time()
 while(True):
-    printscreen_pil = ImageGrab.grab(bbox=(0,40,800,640))
+    screen = np.array(ImageGrab.grab(bbox=(0,40,800,640)))
     ##printscreen_numpy = np.array(printscreen_pil.getdata(),dtype='uint8')
 
     print('Loop took {} seconds'.format(time.time()-last_time))
     last_time = time.time()
-    cv2.imshow('window', cv2.cvtColor(np.array(printscreen_pil), cv2.COLOR_BGR2RGB))
+    cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
