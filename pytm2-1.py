e7f3e17d18072b06f4ce14081db49c53a94255d8
Alexandre JANNAUD <alexandre.jannaud@hotmail.fr>
2017-06-13 15:05:21 +0200

    CV2 - Change color from BGR ro RGB

--- pytm2-1.py
+++ pytm2-1.py
@@ -10,7 +10,7 @@ while(True):
 
     print('Loop took {} seconds'.format(time.time()-last_time))
     last_time = time.time()
-    cv2.imshow('window',np.array(printscreen_pil))
+    cv2.imshow('window', cv2.cvtColor(np.array(printscreen_pil), cv2.COLOR_BGR2RGB))
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
