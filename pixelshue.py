f517151f33f5edc09af54c30253408aae50f51e6
Alexandre Nossent <nossent@et.esiea.fr>
2017-06-19 10:35:06 +0200

    Give HSV ranges as parameter for hueMask

--- pixelshue.py
+++ pixelshue.py
@@ -1,8 +1,7 @@
 import numpy as np
 import cv2
 
-def hueMask (pixels, hueRange) :
-    hsv = [[hueRange[0],0,0],[hueRange[1],255,255]]
-    lower = np.array(hsv[0], dtype = "uint8")
-    upper = np.array(hsv[1], dtype = "uint8")
+def hueMask (pixels, hsvRange) :
+    lower = np.array(hsvRange[0], dtype = "uint8")
+    upper = np.array(hsvRange[1], dtype = "uint8")
     return cv2.inRange(pixels, lower, upper)
--- pytm2-1.py
+++ pytm2-1.py
@@ -2,20 +2,19 @@ import numpy as np
 from PIL import ImageGrab
 import cv2
 import time
-from pixelshue import hueMask 
+from pixelshue import hueMask
+
+hsvRange = [[22,150,150],[38,255,255]]
 
 last_time = time.time()
 while(True):
-    ##color to catch (R:53,G:255,B:255)
     screenBGR = np.array(ImageGrab.grab(bbox=(0,40,800,640)))
-    screenRGB = cv2.cvtColor(screenBGR, cv2.COLOR_BGR2RGB)
+    #screenRGB = cv2.cvtColor(screenBGR, cv2.COLOR_BGR2RGB)
     screenHSV = cv2.cvtColor(screenBGR, cv2.COLOR_BGR2HSV)
-    ##printscreen_numpy = np.array(printscreen_pil.getdata(),dtype='uint8')
 
-    mask = hueMask(screenHSV, [20,40])
+    mask = hueMask(screenHSV, hsvRange)
 
-    output = cv2.bitwise_and(screenRGB, screenRGB, mask = mask)
-    #masked_screen = np.hstack([screen, output])
+    output = cv2.bitwise_and(screenBGR, screenBGR, mask = mask)
     
     print('Loop took {} seconds'.format(time.time()-last_time))
     last_time = time.time()
