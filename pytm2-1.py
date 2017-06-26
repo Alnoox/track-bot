3e38d085b578a643131100f831c0031ed01b5174
Alexandre Nossent <nossent@et.esiea.fr>
2017-06-26 21:33:52 +0200

    Put image processing in separate function

--- pytm2-1.py
+++ pytm2-1.py
@@ -4,17 +4,20 @@ import cv2
 import time
 from pixelshue import hueMask
 
+def processImg(originalImage) :
+    processedImg = cv2.cvtColor(originalImage, cv2.COLOR_BGR2HSV)
+    mask = hueMask(processedImg, hsvRange)
+    processedImg = cv2.bitwise_and(processedImg, processedImg, mask = mask)
+    return processedImg
+
 hsvRange = [[22,150,150],[38,255,255]]
 
 last_time = time.time()
 while(True):
+    #Capture screen
     screenBGR = np.array(ImageGrab.grab(bbox=(0,40,800,640)))
     #screenRGB = cv2.cvtColor(screenBGR, cv2.COLOR_BGR2RGB)
-    screenHSV = cv2.cvtColor(screenBGR, cv2.COLOR_BGR2HSV)
-
-    mask = hueMask(screenHSV, hsvRange)
-
-    output = cv2.bitwise_and(screenBGR, screenBGR, mask = mask)
+    output = processImg(screenBGR)
     
     print('Loop took {} seconds'.format(time.time()-last_time))
     last_time = time.time()
@@ -23,3 +26,4 @@ while(True):
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
+
