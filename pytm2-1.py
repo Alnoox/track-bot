948ea46632db36d8cd4298cabb3d537b1e6a1ff2
Alexandre Nossent <nossent@et.esiea.fr>
2017-06-26 22:09:18 +0200

    Add comments

--- pytm2-1.py
+++ pytm2-1.py
@@ -6,12 +6,18 @@ from pixelshue import hueMask
 from roi import roi
 
 def processImg(originalImage) :
+    #HSV mask
     processedImg = cv2.cvtColor(originalImage, cv2.COLOR_BGR2HSV)
     mask = hueMask(processedImg, hsvRange)
     processedImg = cv2.bitwise_and(processedImg, processedImg, mask = mask)
+
+    #Canny for edge detection
     processedImg = cv2.Canny(processedImg, threshold1=200, threshold2=300)
+
+    #Region Of Interest
     vertices = np.array([[0,225],[0,600],[800,600],[800,225],], np.int32)
     processedImg = roi(processedImg,[vertices])
+
     return processedImg
 
 hsvRange = [[22,150,150],[38,255,255]]
